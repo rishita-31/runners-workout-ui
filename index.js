44f7d10010f9cef723541a5ce351004f4707fc36
rishitaprakash <rishitaprakash3105@gmail.com>
2022-09-28 08:46:06 +0530

    app

---
+++ index.js
@@ -0,0 +1,12 @@
+$('#strength').hover(()=>{
+    $('.strength').toggleClass('col-sm-6');
+    $('#more1').toggleClass('display');
+});
+$('#mobility').hover(()=>{
+    $('.mobility').toggleClass('col-sm-6');
+    $('#more2').toggleClass('display');
+
+});
+$('#drill').hover(()=>{
+    $('#more3').toggleClass('display');
+});
